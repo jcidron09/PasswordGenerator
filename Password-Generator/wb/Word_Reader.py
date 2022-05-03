96bf40cc286498288a0a9103899534d668fd282d
Joshua Cidron <jcidron09@gmail.com>
2022-05-03 19:14:55 -0400

    re-adding

--- Password-Generator/wb/Word_Reader.py
+++ Password-Generator/wb/Word_Reader.py
@@ -1,22 +1,22 @@
 import os
 
+
 def get_word_by_index(index):
     words = read_words()
-    return words[index]
+    return words[index].replace("\r\n", "")
+
 
 def convert_lines():
-  return (" ").join(read_words())
+    return " ".join(read_words())
+
 
 def read_words():
-  os.chdir(os.path.curdir + "/Words")
-  with open("words_alpha.txt", "r") as file:
-      lines= []
-      for line in file:
-          lines.append(line)
-      return lines
-
+    with open("words_alpha.txt", "r") as file:
+        lines = []
+        for line in file:
+            lines.append(line)
+        return lines
 
 
 if __name__ == "__main__":
     print(get_word_by_index(2))
-  
